922b203e6db5b090672f242ae963b02b26e38fba
Kim Doberstein <dobe0002@umn.edu>
2020-01-23 21:00:48 -0600

    Initial session 4 files

---
+++ session_4-linting/sampleFiles-done/src/sample.js
@@ -0,0 +1,91 @@
+/*
+BEFORE:
+
+var returnVars = (...theArgs) => theArgs;
+
+var my_variable = "This is my variable"
+my_variable = "This is no longer my variable"
+returnVars(my_variable)
+
+
+
+
+//Many lines above this comment
+
+
+function MyFunction (param) {
+            var uselessVar, anotherVar;
+            returnVars(uselessVar, anotherVar)
+      if(param=="yay"){returnVars("I said yay!")}
+      else if(param=="boo") returnVars("I said boo!")
+      else {returnVars("well then")}
+}     
+
+MyFunction()
+
+var MyOtherFunction=(param1,param2) => {return "'boo!' "+param1+" "+param2}
+MyOtherFunction('me',"you")
+
+
+
+var myJSON={"key1":"value1","key2":"value2","key3":"value3","key4":"value4","key5":["one","two","three",['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'red', 'orange', 'yellow', 'green', 'blue', 'violet']]}
+returnVars(myJSON)
+console.log(myJSON)
+
+*/
+
+const returnVars = (...theArgs) => theArgs;
+
+let myVariable = 'This is my variable';
+myVariable = 'This is no longer my variable';
+returnVars(myVariable);
+
+// Many lines above this comment
+
+function MyFunction(param) {
+  let uselessVar;
+  let anotherVar;
+  returnVars(uselessVar, anotherVar);
+  if (param === 'yay') {
+    returnVars('I said yay!');
+  } else if (param === 'boo') {
+    returnVars('I said boo!');
+  } else {
+    returnVars('well then');
+  }
+}
+
+MyFunction();
+
+const MyOtherFunction = (param1, param2) => {
+  return `'boo!' ${param1} ${param2}`;
+};
+MyOtherFunction('me', 'you');
+
+const myJSON = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+  key4: 'value4',
+  key5: [
+    'one',
+    'two',
+    'three',
+    [
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet'
+    ]
+  ]
+};
+returnVars(myJSON);
+// console.log(myJSON);
