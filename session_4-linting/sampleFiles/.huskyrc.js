79552e648e7a24f16983ee76827b4e7ed7258e0f
Kim Doberstein <dobe0002@umn.edu>
2020-01-26 21:18:19 -0600

    Adding lint-staged

--- session_4-linting/sampleFiles/.huskyrc.js
+++ session_4-linting/sampleFiles/.huskyrc.js
@@ -2,6 +2,9 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    //'pre-commit': tasks(['npm run lint:all'])
+    'pre-commit': tasks([
+      // 'npm run lint:all' // Use this if you want to check all files before every commit
+      // "lint-staged" // Use this if you want to only check stagged files
+    ])
   }
 };
